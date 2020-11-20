486d065a94cd21821263a2d999f3d9e244c35b12
donnchao <donnchao@outlook.com>
2020-11-20 08:28:13 +0800

    绕过Angular的变更检测

--- src/app/home/components/home-grand.component.ts
+++ src/app/home/components/home-grand.component.ts
@@ -1,4 +1,4 @@
-import { Component, Injectable, InjectionToken, Injector, OnInit } 
+import { ChangeDetectorRef, Component, Injectable, Injector, NgZone, OnInit } 
 from '@angular/core';
 import { token } from '../services/home.service';
 
@@ -28,14 +28,37 @@ class PurchaseOrder {
  * １：浏览器事件如click,mouseover,keyup等
  * ２：setTimeout()和setInterval()
  * 3: HTTP请求
+ * 无法触发变更检测的情况：
+ * １：使用异步方式运行的第三方类库；
+ * ２：不可变数据
+ * ３：可观察对象
  * 
  * 如何进行脏值检测？
  * 检查两个状态值：当前状态和新状态
+ * 
+ * 每个组件都有自己的变更检测器，因此变更检测器同样是一颗树。
+ * 当一个组件发生变更时，无论它发生在什么位置，都会触发树中的所有变更检测器。
+ * Angular会从顶部节点开始，一直扫描到树的叶子节点。
+ * 
+ * OnPush策略：
+ * “傻瓜组件”应该使用OnPush策略　
+ * changeDetection: ChangeDetectionStrategy.OnPush
+ * 只有组件自己的输入属性发生变化时才执行检测
+ * 
+ * 变更检测器：
+ * private changeDetector: ChangeDetectorRef
+ * markForCheck方法告诉Angular已经发生了变化，使变更检测生效
+ * 
+ *　AfterViewInit和AfterViewChecked 钩子在视图初始化完成之后调用，
+ * 不能在这里更改状态值，否则会死循环。
+ * Angular会在视图渲染完之后再次检查数据，如果不同，则会提示错误。
  */
 @Component({
     selector: 'home-grand',
     template: `
-        <p>{{obj|json}}</p>
+        <h3>{{title}}</h3>
+        <p>{{time| date: 'HH:mm:ss'}}</p>
+        <h3>{{obj|json}}</h3>
         <p>{{date|appAgo}}</p>
         <p>{{date|date: 'YYYY年MM月DD日'}}</p>
         <p>{{price|currency: 'CNY': 'symbol':'0.0-2'}}</p>
@@ -46,8 +69,13 @@ export class HomeGrandComponent implements OnInit {
     obj = {productId: 2, productName: 'xx手机', model: 's',type: '全面屏'}
     date: Date;
     price: string;
+    _title: string;
+    _time: number;
     purchaseOrder: PurchaseOrder;
-    constructor() { }
+    constructor(
+        private ngZone: NgZone, 
+        private changeDetection: ChangeDetectorRef
+    ) { }
 
     ngOnInit(): void {
         this.date = this.minuiDays(new Date(), 2);
@@ -78,5 +106,25 @@ export class HomeGrandComponent implements OnInit {
         const reszult = new Date(date);
         reszult.setDate(reszult.getDate() - days);
         return reszult;
+    }
+    public get title():string {
+        return this._title;
+    }
+    public get time(): number {
+        // 如果在这里直接返回Date.now()会报错
+        return this._time;
+    }
+    ngAfterViewChecked(): void {
+        // 绕过Angular的变更检测
+        this.ngZone.runOutsideAngular(()=>{
+            setTimeout(()=>{
+                this._title = '你好';
+                this._time = Date.now();
+                console.log('======');
+            }, 1);
+        });
+        setTimeout(() => {
+            this.changeDetection.markForCheck();
+        }, 1);
     }
 }
